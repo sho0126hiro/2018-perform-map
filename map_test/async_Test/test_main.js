4b16bcd0d780d1c31240f7ed75e03eff383931a8
sho0126hiro <sho0126hiro@yahoo.co.jp>
2018-09-24 01:47:20 +0900

    finish

--- map_test/async_Test/test_main.js
+++ map_test/async_Test/test_main.js
@@ -1,51 +1,36 @@
 window.addEventListener("load",init);
 
-function init() {
+function init(){
   // Canvas / Stage 定義
   var canvasContainer = document.getElementById("wrap");
   var canvasElement = document.getElementById("myCanvas");
-  // サイズ調整
+  // canvasの大きさを画面サイズにする
   var Sizing = function(){
     canvasElement.height = canvasContainer.offsetHeight;
     canvasElement.width  = canvasContainer.offsetWidth;
   };
   Sizing();
-  // stageの宣言
   var stage = new createjs.StageGL(canvasElement);
   stage.setClearColor('#FFFFFF');
-  // 画像の読み込み
+  // bitmap / rect の定義
   var bmp = new createjs.Bitmap("./../imgs/base.png");
-  //画像を画面いっぱいのサイズに
-  SizeMaxBitmap(bmp);
-  // rectの宣言
   var rect = new createjs.Shape();
-  rect.graphics.beginFill("DarkRed"); // 赤色で描画するように設定
-  rect.graphics.drawRect(0, 0, 100, 200);
-  // async - await
-  function GetBitmapWidth(bmp){
-    return new Promise((resolve,reject) => {
-      bmp.image.onload = function(){
-        resolve(bmp.width);
-      };
-    });
-  }
-  async function SizeMaxBitmap(bmp){
-    const bmp1 = GetBitmapWidth(bmp);
-    const width1 = await bmp1;
-    // widthがこの下から使えるぞ
-    bmp.scaleX = canvasElement.width / width1;
-    bmp.scaleY = bmp.scaleX;
-    rect.x = bmp.x * bmp.scaleX + parseInt(420);
-    rect.y =bmp.y * bmp.scale+parseInt(200);
-    rect.alpha = 0.5;
-  }
+  // Containerの定義
   var rectContainer   = new createjs.Container();
-  rectContainer.addChild(rect);
   var DisplayContainer = new createjs.Container();
-  DisplayContainer.addChild(bmp);
-  DisplayContainer.addChild(rectContainer);
-  DisplayContainer.cache(0,0,2000,2000);
-  stage.addChild(DisplayContainer);
+  main();
+  // main
+  async function main(){
+    // 画像のロードを完全に済ませる
+    var bmp_width = await LoadImage(bmp);
+    bmp.scaleX = canvasElement.width / bmp_width;
+    bmp.scaleY = bmp.scaleX;
+    // rectの描画
+    DrawRect();
+    // 親子構造の構築
+    PushContainer();
+  }
+  // Eevnt Listener
   rect.addEventListener("click",handleClick);
   //Resize
   window.addEventListener('resize' , function(){
@@ -55,9 +40,38 @@ function init() {
   createjs.Ticker.on("tick",function(){
     stage.update();
     DisplayContainer.updateCache();
-  });    
+  });
   // Click
   function handleClick(event){
     alert("red Zone");
   } 
+
+  // 画像の読み込み
+  function LoadImage(bitmap){
+    return new Promise(function(resolve,reject){
+      bitmap.image.onload = function(){
+        resolve(this.width);
+      }
+    });
+  }
+
+  // rectの描画
+  function DrawRect(){
+    rect.graphics.beginFill("DarkRed"); // 赤色で描画するように設定
+    rect.graphics.drawRect(0, 0, 100 * bmp.scaleX, 200 * bmp.scaleY);
+    rect.x = bmp.x + parseInt(440) * bmp.scaleX ;
+    rect.y = bmp.y + parseInt(200) * bmp.scaleY;
+    rect.alpha = 0.5;
+  }
+
+  // コンテナの構築
+  function PushContainer(){
+    rectContainer.addChild(rect);
+    DisplayContainer.addChild(bmp);
+    DisplayContainer.addChild(rectContainer);
+    DisplayContainer.cache(0,0,2000,2000);
+    stage.addChild(DisplayContainer);
+  }
+
+  
 }
