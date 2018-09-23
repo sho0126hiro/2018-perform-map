d4cbed7772637593866e356e67a8aa0a95bed4a7
sho0126hiro <sho0126hiro@yahoo.co.jp>
2018-09-24 01:06:37 +0900

    bad

--- map_test/async_Test/test_main.js
+++ map_test/async_Test/test_main.js
@@ -4,22 +4,41 @@ function init() {
   // Canvas / Stage 定義
   var canvasContainer = document.getElementById("wrap");
   var canvasElement = document.getElementById("myCanvas");
+  // サイズ調整
+  var Sizing = function(){
+    canvasElement.height = canvasContainer.offsetHeight;
+    canvasElement.width  = canvasContainer.offsetWidth;
+  };
   Sizing();
+  // stageの宣言
   var stage = new createjs.StageGL(canvasElement);
-  stage.setClearColor('#FFFFFF')
+  stage.setClearColor('#FFFFFF');
+  // 画像の読み込み
   var bmp = new createjs.Bitmap("./../imgs/base.png");
-  var rect = new createjs.Shape();
   //画像を画面いっぱいのサイズに
   SizeMaxBitmap(bmp);
-  //bmp.y=0;
+  // rectの宣言
+  var rect = new createjs.Shape();
   rect.graphics.beginFill("DarkRed"); // 赤色で描画するように設定
   rect.graphics.drawRect(0, 0, 100, 200);
-  //rect.scaleX = bmp.scaleX;
-  //rect.scaleY = bmp.scaleY;
-  rect.x = bmp.x * bmp.scaleX + parseInt(420);
-  rect.y=bmp.y * bmp.scale+parseInt(200);
-  
-  rect.alpha = 0.5;
+  // async - await
+  function GetBitmapWidth(bmp){
+    return new Promise((resolve,reject) => {
+      bmp.image.onload = function(){
+        resolve(bmp.width);
+      };
+    });
+  }
+  async function SizeMaxBitmap(bmp){
+    const bmp1 = GetBitmapWidth(bmp);
+    const width1 = await bmp1;
+    // widthがこの下から使えるぞ
+    bmp.scaleX = canvasElement.width / width1;
+    bmp.scaleY = bmp.scaleX;
+    rect.x = bmp.x * bmp.scaleX + parseInt(420);
+    rect.y =bmp.y * bmp.scale+parseInt(200);
+    rect.alpha = 0.5;
+  }
   var rectContainer   = new createjs.Container();
   rectContainer.addChild(rect);
   var DisplayContainer = new createjs.Container();
@@ -36,23 +55,7 @@ function init() {
   createjs.Ticker.on("tick",function(){
     stage.update();
     DisplayContainer.updateCache();
-  });
-
-  function Sizing(){
-    canvasElement.height = canvasContainer.offsetHeight;
-    canvasElement.width  = canvasContainer.offsetWidth;
-  }
-  function SizeMaxBitmap(bmp){
-    bmp.image.onload = function(){
-      bmp.scaleX = canvasElement.width / this.width;
-      bmp.scaleY = bmp.scaleX;
-      // x,yの位置に画像の中央が表示されるように設定
-      //bmp.regX = bmp.getBounds().width / 2;
-      //bmp.regY = bmp.getBounds().height / 2;
-    };
-  }
-    
-    
+  });    
   // Click
   function handleClick(event){
     alert("red Zone");
